ce2fd2ad4cc31bc41ec3136be03c9a0f628ecedb
treppi <treppi@ahegao.agency>
2023-05-05 14:16:57 +0200

    add multiple prefixes

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const ethers = require('ethers');
-let prefix = "696969";
+const fs = require("fs");
+const nodeCmd = require("node-cmd");
+
+let prefixes = ["696969", "696900", "690069", "0069", "6969", "0000", "aaaa", "000000", "aaaaaa", "444444", "AAAAAA", "111111", "222222", "666666", "999999", "FFFFFF", "123456", "abcdef"];
+let checked = 0;
+let started = Date.now();
 
 while(true) {
     const wallet = ethers.Wallet.createRandom();
     const privateKey = wallet.privateKey;
     const publicKey = wallet.address;
 
-    console.log(publicKey);
-    if(publicKey.startsWith("0x" + prefix)) {
-        console.log("found!");
-        console.log("private key: " + privateKey);
-        return;
-    }
+    checked++;
+    console.log(checked + " " + publicKey);
+    prefixes.forEach(prefix => {
+        if(publicKey.startsWith("0x" + prefix)) {
+            let time = ((Date.now() - started) / 1000);
+
+            nodeCmd.run('echo ' + prefix + " " + privateKey + " " + publicKey + " " + time + " >> wallets.txt", (err, data, stderr) => console.log(data));
+
+            console.log("found!");
+            console.log("private key: " + privateKey);
+            console.log("checked: " + checked);
+            console.log("time: " + time);
+        }
+    });
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+i made this to create ethereum wallets which have a cool prefix.
+
+if you want an address like `0x696900006ba27467555eb562121fac00d24e9dd2` for example
+
+of course the code is slow af and not optimized, also you wouldnt run a nodejs app for that case. But as long as you dont make the searched prefix too complicated it works well.
